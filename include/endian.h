e41ce0a45657bda47d81719fc67982c072d1310d
Zachary Geurts <garbalen@gmx.com>
2025-04-11 14:31:18 -0400

    Add files via upload

---
+++ include/endian.h
@@ -0,0 +1,26 @@
+#ifndef _ENDIAN_H
+#define _ENDIAN_H
+
+#ifdef _WIN32
+#include <winsock2.h>
+
+#define htobe16(x) htons(x)
+#define htole16(x) (x)
+#define be16toh(x) ntohs(x)
+#define le16toh(x) (x)
+
+#define htobe32(x) htonl(x)
+#define htole32(x) (x)
+#define be32toh(x) ntohl(x)
+#define le32toh(x) (x)
+
+#define htobe64(x) htonll(x)
+#define htole64(x) (x)
+#define be64toh(x) ntohll(x)
+#define le64toh(x) (x)
+
+#else
+#include <endian.h>
+#endif
+
+#endif /* _ENDIAN_H */
