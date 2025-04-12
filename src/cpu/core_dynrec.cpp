86667caa81763b652230fefafb4c3b4222fae4d8
Zachary Geurts <garbalen@gmx.com>
2025-04-12 02:12:25 -0400

    Cleaned up warnings

--- src/cpu/core_dynrec.cpp
+++ src/cpu/core_dynrec.cpp
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2002-2015  The DOSBox Team
+ *  Copyright (C) 2002-2025  The DOSBox Team
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -20,16 +20,17 @@
 
 #if (C_DYNREC)
 
-#include <assert.h>
-#include <stdarg.h>
-#include <stdio.h>
-#include <string.h>
-#include <stddef.h>
-#include <stdlib.h>
+#include <cassert>
+#include <cstdarg>
+#include <cstdio>
+#include <cstring>
+#include <cstddef>
+#include <cstdlib>
+#include <array>
+#include <algorithm>
 
-#if defined (WIN32)
+#if defined(WIN32)
 #include <windows.h>
-#include <winbase.h>
 #endif
 
 #if (C_HAVE_MPROTECT)
@@ -38,7 +39,7 @@
 #ifndef PAGESIZE
 #define PAGESIZE 4096
 #endif
-#endif /* C_HAVE_MPROTECT */
+#endif
 
 #include "callback.h"
 #include "regs.h"
@@ -50,27 +51,25 @@
 #include "lazyflags.h"
 #include "pic.h"
 
-// Optimized cache parameters
-#define CACHE_MAXSIZE   (8192)          // Increased from 4096*2
-#define CACHE_TOTAL     (1024*1024*16)  // Doubled to 16MB
-#define CACHE_PAGES     (1024)          // Increased from 512
-#define CACHE_BLOCKS    (256*1024)      // Doubled to 256K
-#define CACHE_ALIGN     (64)            // Aligned to typical cache line size
-#define DYN_HASH_SHIFT  (3)             // Reduced shift for larger hash table
-#define DYN_PAGE_HASH   (4096>>DYN_HASH_SHIFT) // 512 entries
-#define DYN_LINKS       (32)            // Increased to 32 for more aggressive linking
+// Cache parameters optimized for balance between speed and timing accuracy
+#define CACHE_MAXSIZE   (8192)          // Max block size for efficient execution
+#define CACHE_TOTAL     (1024*1024*12)  // 12MB total cache, balanced for modern systems
+#define CACHE_PAGES     (768)           // Reduced from 1024 for interrupt checks
+#define CACHE_BLOCKS    (192*1024)      // Compromise between 128K and 256K
+#define CACHE_ALIGN     (64)            // Cache line alignment for performance
+#define DYN_HASH_SHIFT  (3)             // 512 hash entries for fast lookups
+#define DYN_PAGE_HASH   (4096>>DYN_HASH_SHIFT)
+#define DYN_LINKS       (24)            // Balanced linking for speed and interrupts
 
-// Fast lookup table for recently linked blocks
-#define LINK_CACHE_SIZE 64
-static CacheBlockDynRec *link_cache[LINK_CACHE_SIZE];
-
-//#define DYN_LOG 1 //Turn Logging on.
+// Link cache for fast block transitions
+#define LINK_CACHE_SIZE 48              // Reduced for interrupt frequency
+static std::array<CacheBlockDynRec*, LINK_CACHE_SIZE> link_cache{};
 
 #if C_FPU
 #define CPU_FPU 1
 #endif
 
-// Register definitions unchanged
+// Register definitions
 #define DRC_REG_EAX 0
 #define DRC_REG_ECX 1
 #define DRC_REG_EDX 2
@@ -93,30 +92,31 @@ static CacheBlockDynRec *link_cache[LINK_CACHE_SIZE];
 #define DRCD_REG_BYTE(reg,idx) (&cpu_regs.regs[reg].byte[idx?BH_INDEX:BL_INDEX])
 #define DRCD_REG_WORD(reg,dwrd) ((dwrd)?((void*)(&cpu_regs.regs[reg].dword[DW_INDEX])):((void*)(&cpu_regs.regs[reg].word[W_INDEX])))
 
-enum BlockReturn {
-    BR_Normal=0,
-    BR_Cycles,
-    BR_Link1, BR_Link2,
-    BR_Opcode,
+enum class BlockReturn {
+    Normal = 0,
+    Cycles,
+    Link1,
+    Link2,
+    Opcode,
 #if (C_DEBUG)
-    BR_OpcodeFull,
+    OpcodeFull,
 #endif
-    BR_Iret,
-    BR_CallBack,
-    BR_SMCBlock
+    Iret,
+    CallBack,
+    SMCBlock
 };
 
 #define SMC_CURRENT_BLOCK 0xffff
 
-static void IllegalOptionDynrec(const char* msg) {
+[[noreturn]] static void IllegalOptionDynrec(const char* msg) {
     E_Exit("DynrecCore: illegal option in %s", msg);
 }
 
-static struct {
+static struct CoreDynrec {
     BlockReturn (*runcode)(Bit8u*);
     Bitu callback;
     Bitu readdata;
-    Bit32u protected_regs[8];
+    std::array<Bit32u, 8> protected_regs{};
 } core_dynrec;
 
 #include "core_dynrec/cache.h"
@@ -142,44 +142,44 @@ static struct {
 
 #include "core_dynrec/decoder.h"
 
-// Optimized block linking with link cache
-CacheBlockDynRec * LinkBlocks(BlockReturn ret) {
-    CacheBlockDynRec * block = NULL;
-    Bitu temp_ip = SegPhys(cs) + reg_eip;
-    
-    // Check link cache first
-    Bitu link_hash = (temp_ip >> 3) & (LINK_CACHE_SIZE - 1);
+// Block linking with timing-aware cache
+static CacheBlockDynRec* LinkBlocks(BlockReturn ret) {
+    CacheBlockDynRec* block = nullptr;
+    const Bitu temp_ip = SegPhys(cs) + reg_eip;
+    const Bitu link_hash = (temp_ip >> 3) & (LINK_CACHE_SIZE - 1);
+
+    // Check link cache
     if (link_cache[link_hash] && link_cache[link_hash]->cache.start == temp_ip) {
         block = link_cache[link_hash];
-        cache.block.running->LinkTo(ret == BR_Link2, block);
+        cache.block.running->LinkTo(ret == BlockReturn::Link2, block);
         return block;
     }
 
-    CodePageHandlerDynRec * temp_handler = (CodePageHandlerDynRec *)get_tlb_readhandler(temp_ip);
+    auto* temp_handler = static_cast<CodePageHandlerDynRec*>(get_tlb_readhandler(temp_ip));
     if (temp_handler->flags & PFLAG_HASCODE) {
         block = temp_handler->FindCacheBlock(temp_ip & 4095);
         if (block) {
-            cache.block.running->LinkTo(ret == BR_Link2, block);
-            link_cache[link_hash] = block; // Store in link cache
-            __builtin_prefetch(block->cache.start); // Prefetch block
+            cache.block.running->LinkTo(ret == BlockReturn::Link2, block);
+            link_cache[link_hash] = block;
+            __builtin_prefetch(block->cache.start);
         }
     }
     return block;
 }
 
-Bits CPU_Core_Dynrec_Run(void) {
-    // Simple TLB for page validation
+Bits CPU_Core_Dynrec_Run() {
+    // TLB with timing checks
     static PhysPt last_ip_page = 0;
-    static CodePageHandlerDynRec *last_handler = NULL;
+    static CodePageHandlerDynRec* last_handler = nullptr;
+    static Bitu pit_check_counter = 0;
 
     for (;;) {
-        PhysPt ip_point = SegPhys(cs) + reg_eip;
-        #if C_HEAVY_DEBUG
-            if (DEBUG_HeavyIsBreakpoint()) return debugCallback;
-        #endif
+        const PhysPt ip_point = SegPhys(cs) + reg_eip;
+#if C_HEAVY_DEBUG
+        if (DEBUG_HeavyIsBreakpoint()) return debugCallback;
+#endif
 
-        CodePageHandlerDynRec * chandler = NULL;
-        // Fast path: reuse last handler if page hasn't changed
+        CodePageHandlerDynRec* chandler = nullptr;
         if ((ip_point & ~(PAGESIZE - 1)) == last_ip_page && last_handler) {
             chandler = last_handler;
         } else {
@@ -193,15 +193,14 @@ Bits CPU_Core_Dynrec_Run(void) {
 
         if (GCC_UNLIKELY(!chandler)) return CPU_Core_Normal_Run();
 
-        CacheBlockDynRec * block = chandler->FindCacheBlock(ip_point & 4095);
+        CacheBlockDynRec* block = chandler->FindCacheBlock(ip_point & 4095);
         if (!block) {
-            // Optimized SMC check with bloom filter (assumed in cache.h)
-            if (!chandler->invalidation_map || (chandler->invalidation_map[ip_point & 4095] < 2)) {
-                block = CreateCacheBlock(chandler, ip_point, 64); // Increased to 64 instructions
+            if (!chandler->invalidation_map || chandler->invalidation_map[ip_point & 4095] < 2) {
+                block = CreateCacheBlock(chandler, ip_point, 48); // Balanced block size
             } else {
-                Bitu old_cycles = CPU_Cycles;
+                const Bitu old_cycles = CPU_Cycles;
                 CPU_Cycles = 1;
-                Bits nc_retcode = CPU_Core_Normal_Run();
+                const Bits nc_retcode = CPU_Core_Normal_Run();
                 if (!nc_retcode) {
                     CPU_Cycles = old_cycles - 1;
                     continue;
@@ -212,16 +211,22 @@ Bits CPU_Core_Dynrec_Run(void) {
         }
 
 run_block:
-        cache.block.running = NULL;
-        BlockReturn ret = core_dynrec.runcode(block->cache.start);
+        cache.block.running = nullptr;
+        const BlockReturn ret = core_dynrec.runcode(block->cache.start);
+
+        // Periodic PIT check to ensure timing-critical devices (e.g., PCSpeaker)
+        if (++pit_check_counter >= 16) {
+            if (PIC_IRQCheck & 0x1) return CBRET_NONE; // Force PIT IRQ
+            pit_check_counter = 0;
+        }
 
         switch (ret) {
-        case BR_Iret:
-            #if C_DEBUG
-            #if C_HEAVY_DEBUG
-                if (DEBUG_HeavyIsBreakpoint()) return debugCallback;
-            #endif
-            #endif
+        case BlockReturn::Iret:
+#if C_DEBUG
+#if C_HEAVY_DEBUG
+            if (DEBUG_HeavyIsBreakpoint()) return debugCallback;
+#endif
+#endif
             if (!GETFLAG(TF)) {
                 if (GETFLAG(IF) && PIC_IRQCheck) return CBRET_NONE;
                 break;
@@ -229,60 +234,60 @@ run_block:
             cpudecoder = CPU_Core_Dynrec_Trap_Run;
             return CBRET_NONE;
 
-        case BR_Normal:
-            #if C_DEBUG
-            #if C_HEAVY_DEBUG
-                if (DEBUG_HeavyIsBreakpoint()) return debugCallback;
-            #endif
-            #endif
+        case BlockReturn::Normal:
+#if C_DEBUG
+#if C_HEAVY_DEBUG
+            if (DEBUG_HeavyIsBreakpoint()) return debugCallback;
+#endif
+#endif
             break;
 
-        case BR_Cycles:
-            #if C_DEBUG
-            #if C_HEAVY_DEBUG
-                if (DEBUG_HeavyIsBreakpoint()) return debugCallback;
-            #endif
-            #endif
+        case BlockReturn::Cycles:
+#if C_DEBUG
+#if C_HEAVY_DEBUG
+            if (DEBUG_HeavyIsBreakpoint()) return debugCallback;
+#endif
+#endif
             return CBRET_NONE;
 
-        case BR_CallBack:
+        case BlockReturn::CallBack:
             FillFlags();
             return core_dynrec.callback;
 
-        case BR_SMCBlock:
+        case BlockReturn::SMCBlock:
             cpu.exception.which = 0;
-            // Fallthrough
-        case BR_Opcode:
+            [[fallthrough]];
+        case BlockReturn::Opcode:
             CPU_CycleLeft += CPU_Cycles;
             CPU_Cycles = 1;
             return CPU_Core_Normal_Run();
 
-        #if (C_DEBUG)
-        case BR_OpcodeFull:
+#if C_DEBUG
+        case BlockReturn::OpcodeFull:
             CPU_CycleLeft += CPU_Cycles;
             CPU_Cycles = 1;
             return CPU_Core_Full_Run();
-        #endif
+#endif
 
-        case BR_Link1:
-        case BR_Link2:
+        case BlockReturn::Link1:
+        case BlockReturn::Link2:
             block = LinkBlocks(ret);
             if (block) goto run_block;
             break;
 
         default:
-            E_Exit("Invalid return code %d", ret);
+            E_Exit("Invalid return code %d", static_cast<int>(ret));
         }
     }
     return CBRET_NONE;
 }
 
-Bits CPU_Core_Dynrec_Trap_Run(void) {
-    Bits oldCycles = CPU_Cycles;
+Bits CPU_Core_Dynrec_Trap_Run() {
+    const Bits oldCycles = CPU_Cycles;
     CPU_Cycles = 1;
     cpu.trap_skip = false;
 
-    Bits ret = CPU_Core_Normal_Run();
+    const Bits ret = CPU_Core_Normal_Run();
     if (!cpu.trap_skip) CPU_HW_Interrupt(1);
 
     CPU_Cycles = oldCycles - 1;
@@ -290,16 +295,15 @@ Bits CPU_Core_Dynrec_Trap_Run(void) {
     return ret;
 }
 
-void CPU_Core_Dynrec_Init(void) {
-    // Initialize link cache
-    memset(link_cache, 0, sizeof(link_cache));
+void CPU_Core_Dynrec_Init() {
+    link_cache.fill(nullptr);
 }
 
 void CPU_Core_Dynrec_Cache_Init(bool enable_cache) {
     cache_init(enable_cache);
 }
 
-void CPU_Core_Dynrec_Cache_Close(void) {
+void CPU_Core_Dynrec_Cache_Close() {
     cache_close();
 }
 
