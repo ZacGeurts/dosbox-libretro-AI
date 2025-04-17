8e7c2bb3a682eda78226c73dbc3405a99f6b46d6
Zachary Geurts <garbalen@gmx.com>
2025-04-17 18:54:09 -0400

    Add files via upload

--- src/cpu/core_normal.cpp
+++ src/cpu/core_normal.cpp
@@ -16,8 +16,8 @@
  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
-#include <stdio.h>
-
+#include <cstdio> // For fprintf
+#include <array>  // For C++17 std::array
 #include "dosbox.h"
 #include "mem.h"
 #include "cpu.h"
@@ -37,179 +37,194 @@
 #define LoadMb(off) mem_readb(off)
 #define LoadMw(off) mem_readw(off)
 #define LoadMd(off) mem_readd(off)
-#define LoadMq(off) ((Bit64u)((Bit64u)mem_readd(off+4)<<32 | (Bit64u)mem_readd(off)))
-#define SaveMb(off,val)	mem_writeb(off,val)
-#define SaveMw(off,val)	mem_writew(off,val)
-#define SaveMd(off,val)	mem_writed(off,val)
-#define SaveMq(off,val) {mem_writed(off,val&0xffffffff);mem_writed(off+4,(val>>32)&0xffffffff);}
-#else 
+#define LoadMq(off) ((Bit64u)((Bit64u)mem_readd(off+4) << 32 | (Bit64u)mem_readd(off)))
+#define SaveMb(off, val) mem_writeb(off, val)
+#define SaveMw(off, val) mem_writew(off, val)
+#define SaveMd(off, val) mem_writed(off, val)
+#define SaveMq(off, val) { mem_writed(off, val & 0xffffffff); \
+                           mem_writed(off + 4, (val >> 32) & 0xffffffff); }
+#else
 #include "paging.h"
 #define LoadMb(off) mem_readb_inline(off)
 #define LoadMw(off) mem_readw_inline(off)
 #define LoadMd(off) mem_readd_inline(off)
-#define LoadMq(off) ((Bit64u)((Bit64u)mem_readd_inline(off+4)<<32 | (Bit64u)mem_readd_inline(off)))
-#define SaveMb(off,val)	mem_writeb_inline(off,val)
-#define SaveMw(off,val)	mem_writew_inline(off,val)
-#define SaveMd(off,val)	mem_writed_inline(off,val)
-#define SaveMq(off,val) {mem_writed_inline(off,val&0xffffffff);mem_writed_inline(off+4,(val>>32)&0xffffffff);}
+#define LoadMq(off) ((Bit64u)((Bit64u)mem_readd_inline(off+4) << 32 | (Bit64u)mem_readd_inline(off)))
+#define SaveMb(off, val) mem_writeb_inline(off, val)
+#define SaveMw(off, val) mem_writew_inline(off, val)
+#define SaveMd(off, val) mem_writed_inline(off, val)
+#define SaveMq(off, val) { mem_writed_inline(off, val & 0xffffffff); \
+                           mem_writed_inline(off + 4, (val >> 32) & 0xffffffff); }
 #endif
 
 extern Bitu cycle_count;
 
 #if C_FPU
-#define CPU_FPU	1						//Enable FPU escape instructions
+#define CPU_FPU 1 // Enable FPU escape instructions
 #endif
 
 #define CPU_PIC_CHECK 1
 #define CPU_TRAP_CHECK 1
 
-#define OPCODE_NONE			0x000
-#define OPCODE_0F			0x100
-#define OPCODE_SIZE			0x200
+#define OPCODE_NONE 0x000
+#define OPCODE_0F   0x100
+#define OPCODE_SIZE 0x200
 
-#define PREFIX_ADDR			0x1
-#define PREFIX_REP			0x2
+#define PREFIX_ADDR 0x1
+#define PREFIX_REP  0x2
 
-#define TEST_PREFIX_ADDR	(core.prefixes & PREFIX_ADDR)
-#define TEST_PREFIX_REP		(core.prefixes & PREFIX_REP)
+#define TEST_PREFIX_ADDR (core.prefixes & PREFIX_ADDR)
+#define TEST_PREFIX_REP  (core.prefixes & PREFIX_REP)
 
-#define DO_PREFIX_SEG(_SEG)					\
-	BaseDS=SegBase(_SEG);					\
-	BaseSS=SegBase(_SEG);					\
-	core.base_val_ds=_SEG;					\
-	goto restart_opcode;
+#define DO_PREFIX_SEG(_SEG)                    \
+    BaseDS = SegBase(_SEG);                    \
+    BaseSS = SegBase(_SEG);                    \
+    core.base_val_ds = _SEG;                   \
+    goto restart_opcode;
 
-#define DO_PREFIX_ADDR()								\
-	core.prefixes=(core.prefixes & ~PREFIX_ADDR) |		\
-	(cpu.code.big ^ PREFIX_ADDR);						\
-	core.ea_table=&EATable[(core.prefixes&1) * 256];	\
-	goto restart_opcode;
+#define DO_PREFIX_ADDR()                               \
+    core.prefixes = (core.prefixes & ~PREFIX_ADDR) |   \
+                    (cpu.code.big ^ PREFIX_ADDR);      \
+    core.ea_table = &EATable[(core.prefixes & 1) * 256]; \
+    goto restart_opcode;
 
-#define DO_PREFIX_REP(_ZERO)				\
-	core.prefixes|=PREFIX_REP;				\
-	core.rep_zero=_ZERO;					\
-	goto restart_opcode;
+#define DO_PREFIX_REP(_ZERO)           \
+    core.prefixes |= PREFIX_REP;       \
+    core.rep_zero = _ZERO;             \
+    goto restart_opcode;
 
 typedef PhysPt (*GetEAHandler)(void);
 
-static const Bit32u AddrMaskTable[2]={0x0000ffff,0xffffffff};
+static constexpr std::array<Bit32u, 2> AddrMaskTable = {0x0000ffff, 0xffffffff}; // C++17 constexpr array
 
 static struct {
-	Bitu opcode_index;
-	PhysPt cseip;
-	PhysPt base_ds,base_ss;
-	SegNames base_val_ds;
-	bool rep_zero;
-	Bitu prefixes;
-	GetEAHandler * ea_table;
+    Bitu opcode_index;
+    PhysPt cseip;
+    PhysPt base_ds, base_ss;
+    SegNames base_val_ds;
+    bool rep_zero;
+    Bitu prefixes;
+    GetEAHandler* ea_table;
 } core;
 
-#define GETIP		(core.cseip-SegBase(cs))
-#define SAVEIP		reg_eip=GETIP;
-#define LOADIP		core.cseip=(SegBase(cs)+reg_eip);
+#define GETIP        (core.cseip - SegBase(cs))
+#define SAVEIP       reg_eip = GETIP;
+#define LOADIP       core.cseip = (SegBase(cs) + reg_eip);
 
-#define SegBase(c)	SegPhys(c)
-#define BaseDS		core.base_ds
-#define BaseSS		core.base_ss
+#define SegBase(c)   SegPhys(c)
+#define BaseDS       core.base_ds
+#define BaseSS       core.base_ss
 
-static INLINE Bit8u Fetchb() {
-	Bit8u temp=LoadMb(core.cseip);
-	core.cseip+=1;
-	return temp;
+static inline Bit8u Fetchb() {
+    Bit8u temp = LoadMb(core.cseip);
+    core.cseip += 1;
+    return temp;
 }
 
-static INLINE Bit16u Fetchw() {
-	Bit16u temp=LoadMw(core.cseip);
-	core.cseip+=2;
-	return temp;
+static inline Bit16u Fetchw() {
+    Bit16u temp = LoadMw(core.cseip);
+    core.cseip += 2;
+    return temp;
 }
-static INLINE Bit32u Fetchd() {
-	Bit32u temp=LoadMd(core.cseip);
-	core.cseip+=4;
-	return temp;
+
+static inline Bit32u Fetchd() {
+    Bit32u temp = LoadMd(core.cseip);
+    core.cseip += 4;
+    return temp;
 }
 
 #define Push_16 CPU_Push16
 #define Push_32 CPU_Push32
-#define Pop_16 CPU_Pop16
-#define Pop_32 CPU_Pop32
+#define Pop_16  CPU_Pop16
+#define Pop_32  CPU_Pop32
 
 #include "instructions.h"
 #include "core_normal/support.h"
 #include "core_normal/string.h"
 
-
 #define EALookupTable (core.ea_table)
 
 Bits CPU_Core_Normal_Run(void) {
-	while (CPU_Cycles-->0) {
-		LOADIP;
-		core.opcode_index=cpu.code.big*0x200;
-		core.prefixes=cpu.code.big;
-		core.ea_table=&EATable[cpu.code.big*256];
-		BaseDS=SegBase(ds);
-		BaseSS=SegBase(ss);
-		core.base_val_ds=ds;
+    fprintf(stderr, "[CPU] CPU_Core_Normal_Run started, cycles=%lx\n", (unsigned long)CPU_Cycles);
+
+    while (CPU_Cycles-- > 0) {
+        LOADIP;
+        core.opcode_index = cpu.code.big * 0x200;
+        core.prefixes = cpu.code.big;
+        core.ea_table = &EATable[cpu.code.big * 256];
+        BaseDS = SegBase(ds);
+        BaseSS = SegBase(ss);
+        core.base_val_ds = ds;
 #if C_DEBUG
 #if C_HEAVY_DEBUG
-		if (DEBUG_HeavyIsBreakpoint()) {
-			FillFlags();
-			return debugCallback;
-		};
+        if (DEBUG_HeavyIsBreakpoint()) {
+            FillFlags();
+            return debugCallback;
+        }
 #endif
-		cycle_count++;
+        cycle_count++;
 #endif
 restart_opcode:
-		switch (core.opcode_index+Fetchb()) {
-		#include "core_normal/prefix_none.h"
-		#include "core_normal/prefix_0f.h"
-		#include "core_normal/prefix_66.h"
-		#include "core_normal/prefix_66_0f.h"
-      #include "core_normal/prefix_0f_mmx.h"
-		default:
-		illegal_opcode:
-#if C_DEBUG	
-			{
-				Bitu len=(GETIP-reg_eip);
-				LOADIP;
-				if (len>16) len=16;
-				char tempcode[16*2+1];char * writecode=tempcode;
-				for (;len>0;len--) {
-					sprintf(writecode,"%02X",mem_readb(core.cseip++));
-					writecode+=2;
-				}
-				LOG(LOG_CPU,LOG_NORMAL)("Illegal/Unhandled opcode %s",tempcode);
-			}
+        Bit8u opcode = Fetchb();
+        // Handle two-byte opcodes separately to reduce switch complexity
+        if (opcode == 0x0F) {
+            core.opcode_index |= OPCODE_0F;
+            opcode = Fetchb();
+        }
+
+        switch (core.opcode_index + opcode) {
+        #include "core_normal/prefix_none.h"
+        #include "core_normal/prefix_0f.h"
+        #include "core_normal/prefix_66.h"
+        #include "core_normal/prefix_66_0f.h"
+        #include "core_normal/prefix_0f_mmx.h"
+        default:
+        illegal_opcode:
+#if C_DEBUG
+            {
+                Bitu len = (GETIP - reg_eip);
+                LOADIP;
+                if (len > 16) len = 16;
+                char tempcode[16 * 2 + 1];
+                char* writecode = tempcode;
+                for (; len > 0; len--) {
+                    snprintf(writecode, 3, "%02X", mem_readb(core.cseip++));
+                    writecode += 2;
+                }
+                fprintf(stderr, "[CPU] Illegal/Unhandled opcode %s at cseip=%lx\n", tempcode, (unsigned long)core.cseip);
+                LOG(LOG_CPU, LOG_NORMAL)("Illegal/Unhandled opcode %s", tempcode);
+            }
 #endif
-			CPU_Exception(6,0);
-			continue;
-		}
-		SAVEIP;
-	}
-	FillFlags();
-	return CBRET_NONE;
+            CPU_Exception(6, 0);
+            continue;
+        }
+        SAVEIP;
+    }
+    FillFlags();
+    fprintf(stderr, "[CPU] CPU_Core_Normal_Run ended, cycles=%lx\n", (unsigned long)CPU_Cycles);
+    return CBRET_NONE;
+
 decode_end:
-	SAVEIP;
-	FillFlags();
-	return CBRET_NONE;
+    SAVEIP;
+    FillFlags();
+    return CBRET_NONE;
 }
 
 Bits CPU_Core_Normal_Trap_Run(void) {
-	Bits oldCycles = CPU_Cycles;
-	CPU_Cycles = 1;
-	cpu.trap_skip = false;
+    fprintf(stderr, "[CPU] CPU_Core_Normal_Trap_Run started, cycles=%lx\n", (unsigned long)CPU_Cycles);
 
-	Bits ret=CPU_Core_Normal_Run();
-	if (!cpu.trap_skip) CPU_HW_Interrupt(1);
-	CPU_Cycles = oldCycles-1;
-	cpudecoder = &CPU_Core_Normal_Run;
+    Bits oldCycles = CPU_Cycles;
+    CPU_Cycles = 1;
+    cpu.trap_skip = false;
 
-	return ret;
+    Bits ret = CPU_Core_Normal_Run();
+    if (!cpu.trap_skip) CPU_HW_Interrupt(1);
+    CPU_Cycles = oldCycles - 1;
+    cpudecoder = &CPU_Core_Normal_Run;
+
+    fprintf(stderr, "[CPU] CPU_Core_Normal_Trap_Run ended, return=%lx\n", (unsigned long)ret);
+    return ret;
 }
-
-
 
 void CPU_Core_Normal_Init(void) {
-
+    fprintf(stderr, "[CPU] CPU_Core_Normal_Init called\n");
 }
-
