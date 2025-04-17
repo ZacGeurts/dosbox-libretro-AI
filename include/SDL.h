be9594b49f5c77a5610804f073ad78bbdef23c83
Zachary Geurts <garbalen@gmx.com>
2025-04-16 23:18:53 -0400

    Add files via upload

--- include/SDL.h
+++ include/SDL.h
@@ -1,9 +1,10 @@
 #ifndef SDL_FAKE_
 #define SDL_FAKE_
 
+#include <retro_timers.h> // Added for retro_sleep
 #include <string.h>
 #include <stdint.h>
-#include <sys/time.h>
+//#include <sys/time.h>
 #include <unistd.h>
 
 #include <retro_miscellaneous.h>
@@ -53,12 +54,12 @@ struct SDL_CD {int dummy;};
 /*@{*/
 #define CD_FPS	75
 #define FRAMES_TO_MSF(f, M,S,F)	{					\
-	int value = f;							\
-	*(F) = value%CD_FPS;						\
-	value /= CD_FPS;						\
-	*(S) = value%60;						\
-	value /= 60;							\
-	*(M) = value;							\
+    int value = f;							\
+    *(F) = value%CD_FPS;						\
+    value /= CD_FPS;						\
+    *(S) = value%60;						\
+    value /= 60;							\
+    *(M) = value;							\
 }
 #define MSF_TO_FRAMES(M, S, F)	((M)*60*CD_FPS+(S)*CD_FPS+(F))
 /*@}*/
